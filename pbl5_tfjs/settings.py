46c683af70ed1fb352bc3aaf5e4b57038104f4a5
= <=>
2022-06-14 16:27:13 +0700

    fix library 3

--- pbl5_tfjs/settings.py
+++ pbl5_tfjs/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -127,6 +126,3 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-
-
-django_heroku.settings(locals())
